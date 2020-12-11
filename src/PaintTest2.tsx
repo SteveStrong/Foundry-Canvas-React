6732527d881cb8969a36d008d9a6871421c7b1af
SteveStrong <sstrong@apprenticesystems.com>
2020-12-11 01:18:03 -0500

    now with lights

--- src/PaintTest2.tsx
+++ src/PaintTest2.tsx
@@ -1,4 +1,7 @@
 // https://medium.com/@pdx.lucasm/canvas-with-react-js-32e133c05258
+// https://github.com/elchininet/ColorTranslator
+
+// https://elchininet.github.io/ColorTranslator/
 
 import { Canvas } from 'Canvas';
 import { foObject } from 'foundry/models/foObject.model';
@@ -6,7 +9,10 @@ import { foPage } from 'foundry/models/foPage.model';
 import { hub } from './models/hub';
 
 import React, { FunctionComponent, ReactElement } from 'react';
-import { LEDLight, LightArray } from 'models/lights';
+import { ColorArray, LEDLight, LightArray } from 'models/lights';
+import { ToJSON } from 'core/foRenderer';
+import { Tools } from 'foundry/models/foTools';
+import { ColorTranslator } from 'colortranslator';
 
 
 
@@ -26,13 +32,31 @@ export const PaintTest2: FunctionComponent<any> = (props: any): ReactElement => 
     });
 
 
-    const LEDArray = new LightArray({
-        x: 0,
+    const LEDArrayH = new LightArray({
+        opacity: 0.1,
+        x: page2.width / 2,
         y: page2.height / 2
-    });
+    })
+        .horizontal(LEDLight, { color: 'orange', opacity: 0.4 })
+        .resetSize(20);
+    
+    const mult = 5;
+     const blends = ColorTranslator.getBlendHEX('#FF00FF', '#FFFF00', mult + 10 * mult);
 
-    LEDArray.components();
+    const LEDArrayV = new LightArray({
+        angle: 90,
+        x: page2.width / 2,
+        y: page2.height * 2/3
+    })
+        .vertical(LEDLight, { color: 'red', opacity: 0.4 })
+        .resetSize(13);
 
+    const ColorArrayV = new ColorArray({
+        colors: blends,
+        x: page2.width / 2,
+        y: page2.height / 3
+    }).horizontal(LEDLight);
+    
     const canvas2Params = {
         width: 1500,
         height: 800,
@@ -40,13 +64,37 @@ export const PaintTest2: FunctionComponent<any> = (props: any): ReactElement => 
         draw: (ctx: CanvasRenderingContext2D, count: number) => {
             page2.render(ctx);
             shape2.render(ctx);
-            LEDArray.render(ctx);
+            LEDArrayH.render(ctx);
+            LEDArrayV.render(ctx);
+            ColorArrayV.render(ctx);
         }
     };
+
+
+
+    // const container = document.createElement('div');
+    // const rows = 11;
+    // const mult = 3;
+
+    // for (let i = 0; i < rows; i++) {
+    //     const blends = ColorTranslator.getBlendHEX('#FF0000', '#FFFF00', mult + i * mult);
+
+    //     blends.forEach((blend, index) => {
+    //         const box = document.createElement('div');
+    //         box.classList.add('box', `file${i}`);
+    //         box.style.background = blend;
+    //         box.innerText = index + 1;
+    //         container.appendChild(box);
+    //     });
+    // }
+
+    // return container;
+
 
     return (
         <div>
             <Canvas {...canvas2Params} />
+            <ToJSON {...Tools.asJson(ColorArrayV)} />
         </div>
     );
 };
--- src/models/lights.ts
+++ src/models/lights.ts
@@ -1,27 +1,118 @@
-import { foShape2D } from "foundry/models/foShape2D.model";
+import { foObject } from "foundry/models/foObject.model";
+import { foShape2D, IfoShape2DProperties } from "foundry/models/foShape2D.model";
 
+// function create<T>(c: { new(): T }): T {
+//     return new c();
+// }
+
+export type Newable<T> = { new(...args: any[]): T; };
 
 export class LEDLight extends foShape2D {
     color: string = 'blue';
-    opacity: number = 3.0;
-    _width: number = 50;
+    opacity: number = 0.3;
+    _width: number = 25;
     _height: number = 50;
+
+    constructor(properties?: IfoShape2DProperties, parent?: foObject) {
+        super(properties, parent);
+
+        this.override(properties);
+    }
 }
 
 
-export class LightArray extends foShape2D {
-    total: number = 30;
-    components() {
-        if (this.subcomponents.length === 0) {    
+
+export interface ILightArray2DProperties extends IfoShape2DProperties {
+    total?: number;
+}
+
+export class LightArray<T extends LEDLight> extends foShape2D implements ILightArray2DProperties {
+    opacity: number = 0.2;
+    total: number;
+    private _rebuild: any;
+ 
+
+    constructor(properties?: ILightArray2DProperties, parent?: foObject) {
+        super(properties, parent);
+
+        this.override(properties);
+    }
+
+    clear() {
+        this.subcomponents.clearAll();
+    }
+    resetSize(i: number) {
+        this.total = i;
+        this._rebuild && this._rebuild();
+        return this;
+    }
+    horizontal(c: { new(props?: IfoShape2DProperties): T }, props?: IfoShape2DProperties) {
+
+        this._rebuild = () => { this.horizontal(c, props) };
+        if (this.subcomponents.length !== this.total) {
+            const source = new c(props);
+            this.width = source.width * this.total;
+            this.height = source.height;
+
             for (let i = 0; i < this.total; i++) {
                 const led = new LEDLight({
                     index: i,
-                    x: i * 50,
-                    y: i
+                    x: i * (source.width) + (source.width / 2),
+                    y: (source.height / 2),
+                    ...props,
                 });
                 this.subcomponents.addMember(led);
-            }   
+            }
         }
-        return this.subcomponents
+        return this;
     }
+    vertical(c: { new(props?: IfoShape2DProperties): T }, props?: IfoShape2DProperties) {
+
+        this._rebuild = () => { this.vertical(c, props) };
+        if (this.subcomponents.length !== this.total) {
+            const source = new c(props);
+            this.width = source.width;
+            this.height = source.height * this.total;
+
+            for (let i = 0; i < this.total; i++) {
+                const led = new LEDLight({
+                    index: i,
+                    x: (source.width / 2),
+                    y: i * (source.height) + (source.height / 2),
+                    ...props,
+                });
+                this.subcomponents.addMember(led);
+            }
+        }
+        return this;
+    }
+}
+
+export class ColorArray<T extends LEDLight> extends LightArray<T> implements ILightArray2DProperties {
+    colors: any[];
+
+    constructor(properties?: ILightArray2DProperties, parent?: foObject) {
+        super(properties, parent);
+
+        this.override(properties);
+        this.total = this.colors.length;
+    }
+
+    horizontal(c: { new(props?: IfoShape2DProperties): T }, props?: IfoShape2DProperties) {
+        super.horizontal(c, props);
+        for (let i = 0; i < this.total; i++) {
+            const item = this.subcomponents.getMember(i);
+            item.color = this.colors[i];
+        }
+        return this;
+    }
+    vertical(c: { new(props?: IfoShape2DProperties): T }, props?: IfoShape2DProperties) {
+        super.vertical(c, props);
+        for (let i = 0; i < this.total; i++){
+            const item = this.subcomponents.getMember(i);
+            item.color = this.colors[i];
+        }
+        return this;
+    }
+
 }
